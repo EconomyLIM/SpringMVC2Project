f2106ce74968dceadf77d94d18139c3eb5c84932
EconomyLIM <bmw4117@entropykorea.com>
2024-11-06 10:26:23 +0900

    Initial commit

---
+++ src/main/java/springmvc2/alltest/AlltestApplication.java
@@ -0,0 +1,13 @@
+package springmvc2.alltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlltestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlltestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springmvc2/alltest/AlltestApplicationTests.java
@@ -0,0 +1,13 @@
+package springmvc2.alltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlltestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
